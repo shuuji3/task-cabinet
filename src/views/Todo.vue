57d86b7757ae5da650f6583b7c5ec63ff2e92158
TAKAHASHI Shuuji <shuuji3@gmail.com>
2019-08-01 16:18:39 +0900

    期限によるソートを実装

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -2,7 +2,7 @@
   <v-container>
     <v-layout text-center wrap>
       <v-flex md-12>
-        <v-card v-for="todo in todos" :key="todo.id">
+        <v-card v-for="todo in sortedTodos" :key="todo.id">
           <v-card-title>{{ todo.name }}</v-card-title>
           <v-card-text>期限: {{ todo.deadline.format('YYYY-MM-DD HH:mm') }}</v-card-text>
         </v-card>
@@ -16,6 +16,11 @@ import moment from 'moment';
 import uuid from 'uuid/v4';
 
 export default {
+  computed: {
+    sortedTodos() {
+      return this.todos.slice().sort((todo1, todo2) => todo1.deadline.diff(todo2.deadline));
+    },
+  },
   data() {
     return {
       todos: [
